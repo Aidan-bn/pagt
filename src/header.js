0d463b64ef828b15ca559099922dc67745deff0d
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2022-11-24 10:59:38 +0300

    Update header contents

--- src/header.js
+++ src/header.js
@@ -4,25 +4,25 @@ import './header.css';
 function Header(){
     return(
         <header>
-      <div className='logo'>
-        <img src={logopag} alt='img logo'></img>
-      </div>
-      <nav>
-        <ul className='nav-ul'>
-          <li className='nav-li'>Mwanzo</li>
-          <li className='nav-li'>Kanuni Ya Imani</li>
-          <li className='nav-li'>Idara</li>
-          <li className='nav-li'>Majimbo</li>
-          <li className='nav-li'>Mawasiliano</li>
-        </ul>
-        <div className='hamburger'>
-          <ul className='ul-hamburger'>
-            <li className='img-hamburger'>Img</li>
-            <li className='cross-hamburger'>X</li>
-          </ul>
-        </div>
-      </nav>
-    </header>
+            <div className='logo'>
+                <img src={logopag} alt='img logo' className='logo-pag'></img>
+            </div>
+            <nav>
+                <ul className='nav-ul'>
+                <li className='nav-li'>Mwanzo</li>
+                <li className='nav-li'>Kanuni</li>
+                <li className='nav-li'>Idara</li>
+                <li className='nav-li'>Majimbo</li>
+                <li className='nav-li'>Wasiliana</li>
+                </ul>
+                <div className='hamburger'>
+                <ul className='ul-hamburger'>
+                    <li className='img-hamburger'>Img</li>
+                    <li className='cross-hamburger'>X</li>
+                </ul>
+                </div>
+            </nav>
+        </header>
     )
 }
 export default Header;
