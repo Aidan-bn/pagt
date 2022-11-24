f8271d5c2db2115540dc839e4fc681604ca5527e
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2022-11-24 13:44:31 +0300

    Update allignment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App';
 import Header from './header';
+import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     <Header />
   </React.StrictMode>
 );
