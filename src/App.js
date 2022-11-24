93dccfb5741e5cc709224d9f053b8a6612690a56
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2022-11-24 15:06:19 +0300

    Update landing page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 function App() {
   return (
     <section className='landing'>
-      <h1>PAG TANZANIA</h1>
+      <h1>PENTECOSTAL ASSEMBLIES OF GOD <br></br>(TANZANIA)</h1>
+      <div className='animate'>
+       <p>" Ni wakati wa mambo makubwa sasa</p>
+       <p>" Ni wakati wa mambo makubwa sasa</p>
+       <p>" Ni wakati wa mambo makubwa sasa</p>
+      </div>
     </section>
-    
   );
 }
 
