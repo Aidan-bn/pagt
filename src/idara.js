a07c603b1316e9eb6a943f878b7727be9ac7a973
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2022-11-29 15:30:50 +0300

    update idara section

--- src/idara.js
+++ src/idara.js
@@ -1,9 +1,78 @@
 import './idara.css';
+import james from './asset/james.png';
+import getu from './asset/getu.png';
+import chambo from './asset/chambo.png';
+import jose from './asset/jose.png';
+import thomas from './asset/thomas.png';
+import vice from './asset/vice.png';
 
 function Idara() {
     return (
         <div className='idara'>
-            Hello
+           <h2>Idara Za Kanisa</h2>
+           <ul className='ul-idara'>
+                <li className='li-idara'>
+                    <figure>
+                        <img src={ vice } alt="vice" />
+                    </figure>
+                    <p className='p-idara'><em>Idara ya Makanisa <br /></em>Katika Uungu wa Bwana wetu Yesu Kristo,katika kuzaliwa kwake na bikira huyo ni Mariamu, 
+                        katika maisha yake yasiyo na dhambi,katika miujiza yake,katika kifo chake badala ya wenye 
+                        dhambi ambacho kilifanyika upatanisho kati ya mwanadamu na Mungu, na katika ufufuo wake 
+                        katika mwili na kupaa kwake mbinguni na kukaa mkono wa kuume wa Mungu Baba,na katika 
+                        kurudi kwake mara ya pili hapa duniani katika nguvu na utukufu ili kuyatawala Mataifa</p>
+                </li>
+                <li className='li-idara'>
+                    <p className='p-idara'><em>Idara ya Makanisa <br /></em>Katika Uungu wa Bwana wetu Yesu Kristo,katika kuzaliwa kwake na bikira huyo ni Mariamu, 
+                        katika maisha yake yasiyo na dhambi,katika miujiza yake,katika kifo chake badala ya wenye 
+                        dhambi ambacho kilifanyika upatanisho kati ya mwanadamu na Mungu, na katika ufufuo wake 
+                        katika mwili na kupaa kwake mbinguni na kukaa mkono wa kuume wa Mungu Baba,na katika 
+                        kurudi kwake mara ya pili hapa duniani katika nguvu na utukufu ili kuyatawala Mataifa</p>
+                    <figure>
+                        <img src={ getu } alt="vice" />
+                    </figure>
+                </li>
+                <li className='li-idara'>
+                    <figure>
+                        <img src={ jose } alt="vice" />
+                    </figure>
+                    <p className='p-idara'><em>Idara ya Makanisa <br /></em>Katika Uungu wa Bwana wetu Yesu Kristo,katika kuzaliwa kwake na bikira huyo ni Mariamu, 
+                        katika maisha yake yasiyo na dhambi,katika miujiza yake,katika kifo chake badala ya wenye 
+                        dhambi ambacho kilifanyika upatanisho kati ya mwanadamu na Mungu, na katika ufufuo wake 
+                        katika mwili na kupaa kwake mbinguni na kukaa mkono wa kuume wa Mungu Baba,na katika 
+                        kurudi kwake mara ya pili hapa duniani katika nguvu na utukufu ili kuyatawala Mataifa</p>
+                </li>
+                <li className='li-idara'>
+                    <p className='p-idara'><em>Idara ya Makanisa <br /></em>Katika Uungu wa Bwana wetu Yesu Kristo,katika kuzaliwa kwake na bikira huyo ni Mariamu, 
+                        katika maisha yake yasiyo na dhambi,katika miujiza yake,katika kifo chake badala ya wenye 
+                        dhambi ambacho kilifanyika upatanisho kati ya mwanadamu na Mungu, na katika ufufuo wake 
+                        katika mwili na kupaa kwake mbinguni na kukaa mkono wa kuume wa Mungu Baba,na katika 
+                        kurudi kwake mara ya pili hapa duniani katika nguvu na utukufu ili kuyatawala Mataifa</p>
+                    <figure>
+                        <img src={ thomas } alt="vice" />
+                    </figure>
+                </li>
+                <li className='li-idara'>
+                    <figure>
+                        <img src={ chambo } alt="vice" />
+                    </figure>
+                    <p className='p-idara'><em>Idara ya Makanisa <br /></em>Katika Uungu wa Bwana wetu Yesu Kristo,katika kuzaliwa kwake na bikira huyo ni Mariamu, 
+                        katika maisha yake yasiyo na dhambi,katika miujiza yake,katika kifo chake badala ya wenye 
+                        dhambi ambacho kilifanyika upatanisho kati ya mwanadamu na Mungu, na katika ufufuo wake 
+                        katika mwili na kupaa kwake mbinguni na kukaa mkono wa kuume wa Mungu Baba,na katika 
+                        kurudi kwake mara ya pili hapa duniani katika nguvu na utukufu ili kuyatawala Mataifa</p>
+                </li>
+                <li className='li-idara'>
+                    <p className='p-idara'><em>Idara ya Makanisa <br /></em>Katika Uungu wa Bwana wetu Yesu Kristo,katika kuzaliwa kwake na bikira huyo ni Mariamu, 
+                        katika maisha yake yasiyo na dhambi,katika miujiza yake,katika kifo chake badala ya wenye 
+                        dhambi ambacho kilifanyika upatanisho kati ya mwanadamu na Mungu, na katika ufufuo wake 
+                        katika mwili na kupaa kwake mbinguni na kukaa mkono wa kuume wa Mungu Baba,na katika 
+                        kurudi kwake mara ya pili hapa duniani katika nguvu na utukufu ili kuyatawala Mataifa</p>
+                    <figure>
+                        <img src={ james } alt="vice" />
+                    </figure>
+                </li>
+           </ul>
+           
         </div>
     );
 }
