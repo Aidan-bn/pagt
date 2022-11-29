be7198c128c0106fcc05b356271c0fc1fd75e16f
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2022-11-29 18:01:21 +0300

    Update social media section

--- src/Social.js
+++ src/Social.js
@@ -1,14 +1,17 @@
 import './social.css';
 import fb from './asset/facebook.svg';
-import tw from './asset/twitter.png'
+import tw from './asset/twitter.svg';
+import yt from './asset/youtube.svg';
+import wp from './asset/mob.svg';
 
 function Social() {
     return (
         <div className='social'>
             <ul className='ul-social'>
                 <li className='li-social'><img src={ fb } alt="facebook"  className='img-media'/></li>
-                <li className='li-social'><img src={ tw } alt="facebook"  className='img-media'/></li>
-                <li className='li-social'>WAP</li>
+                <li className='li-social'><img src={ tw } alt="twitter"  className='img-media'/></li>
+                <li className='li-social'><img src={ yt } alt="youtube"  className='img-media'/></li>
+                <li className='li-social'><img src={ wp } alt="Whatsapp"  className='img-media'/></li>
             </ul>
         </div>
     );
