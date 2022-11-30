b57cce002d72914250ada9161d3a9cb0a79c8130
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2022-11-30 15:25:57 +0300

    Update history section

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ function App() {
     <section className='landing'>
       <h1>PENTECOSTAL ASSEMBLIES OF GOD (TANZANIA)</h1>
       <div className='animate'>
-       <p>" Kanisa la Kipentekoste lenye msingi na imani katika Neno la Mungu linaloundwa na ujumla wa makanisa mahalia pamoja. Lilianzishwa chini ya wamisionari kutoka Kanada wakiwa chini ya Shirika la kimisionari liitwalo Pentecostal Assemblies of Canada ambao walianza kueneza Injili Tanganyika katika miaka ya 1912 ambapo katika miaka hiyo wengine walifariki kwa magonjwa.</p><br></br>
-       <p>Mnamo mwaka 1950, wahubiri wa mwanzo wa Tanganyika walipewa mafunzo. Kanisa lilianza katika mkoa wa Mara na kuanzisha shule ya Biblia pale Nyasirori Butiama. Wakati huo lengo kuu lilikuwa kuhubiri Injili na kuanzisha makanisa. Kanisa liandikishwe rasmi mnamo tarehe 14 Aprili mwaka 1967 na kupata "Cheti cha Ithibati"</p>
+       <p>" Kanisa la Kipentekoste lenye msingi na imani katika Neno 
+        la Mungu linaloundwa na ujumla wa makanisa mahalia pamoja.
+         Lilianzishwa chini ya wamisionari kutoka Kanada wakiwa chini 
+         ya Shirika la kimisionari liitwalo Pentecostal Assemblies of 
+         Canada ambao walianza kueneza Injili Tanganyika katika miaka 
+         ya 1912 ambapo katika miaka hiyo wengine walifariki kwa 
+         magonjwa.</p><br></br>
+       <p>Mnamo mwaka 1950, wahubiri wa mwanzo wa Tanganyika walipewa
+         mafunzo. Kanisa lilianza katika mkoa wa Mara na kuanzisha 
+         shule ya Biblia pale Nyasirori Butiama. Wakati huo lengo kuu 
+         lilikuwa kuhubiri Injili na kuanzisha makanisa. Kanisa 
+         liandikishwe rasmi mnamo tarehe 14 Aprili mwaka 1967 na 
+         kupata "Cheti cha Ithibati"</p>
+         <h3>Ni wakati wa mambomakubwa, Inuka uangaze</h3>
       </div>
     </section>
   );
